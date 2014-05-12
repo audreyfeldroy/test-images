95753347b1ae1a2df7a67b227bf06b2227aee901
Audrey Roy <audreyr@gmail.com>
2014-05-12 11:02:01 -0700

    Create a yellow 400x400 png too.

--- scripts/create_png.py
+++ scripts/create_png.py
@@ -4,3 +4,6 @@
 from PIL import Image
 img = Image.new('RGB', (400, 400), (127, 127, 127))
 img.save('../test-images/gray.png')
+
+img = Image.new('RGB', (400, 400), (255, 255, 0))
+img.save('../test-images/yellow.png')
